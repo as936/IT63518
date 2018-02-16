4de437c16ad2a80d385eae575947fe45955b4d4b
Ubuntu <ubuntu@ip-172-31-46-164.us-east-2.compute.internal>
2018-02-16 05:05:25 +0000

    signin.php updates

--- sample/signin.php
+++ sample/signin.php
@@ -1,14 +1,14 @@
 <?php
-include("pdoConn.php")
 session_start();
+include("pdoConn.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST") //this is looking at the html and getting the username and password from form
 {
-	$username = mysqli_real_escape_string($db,$_POST['username']
-	$password = mysqli_real_escape_string($db,$_POST['password']
+	$username = mysqli_real_escape_string($db,$_POST['username']);
+	$password = mysqli_real_escape_string($db,$_POST['password']);
 	
 	$sqlquery = "Select * from users where name = '$username' and password = '$password'";
-	$result = mysqli_query($db,$sqlquery)
+	$result = mysqli_query($db,$sqlquery);
 	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 	
 	$count = mysqli_num_rows($result);
@@ -19,7 +19,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") //this is looking at the html and getti
 		$_SESSION['login_user'] = $username;
 		
 		$success = "Successfully Logged In";
-		printf $success
+
 		header("location: welcomepage.php"); //sending to the landing page after you log in
 	}
 		
